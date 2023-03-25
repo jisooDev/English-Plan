44eee6a214466049a41e485dcb9fca6154c0e91d
jisooDev <phattarakorndev@gmail.com>
2023-03-25 20:39:11 +0700

    merge

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,16 @@
 from flask import Blueprint, session, redirect, url_for, render_template , request, Response
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyDExn5Hbtc_w4IT2-hUe-VT7XE7OkIT5b8",
+    "authDomain": "english-plan-c76bf.firebaseapp.com",
+    "databaseURL": "https://english-plan-c76bf.firebaseapp.com",
+    "projectId": "english-plan-c76bf",
+    "storageBucket": "english-plan-c76bf.appspot.com",
+    "messagingSenderId": "806298914847",
+    "appId": "1:806298914847:web:7fe7508345e15b62a0452a",
+    "measurementId": "G-7PWV72Z2QX"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
--- routes/listening.py
+++ routes/listening.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from . import *
 
 blueprintListening = Blueprint('listening', __name__)
@@ -6,6 +7,15 @@ blueprintListening = Blueprint('listening', __name__)
 @blueprintListening.route('/listening/writedown')
 def render_selectword():
     return render_template('admin/pages/listening/writedown.html')
+
+@blueprintListening.route('/listening/writedown/create', methods=['POST'])
+def create_selectword():
+    with open('test_audio.mp3', 'rb') as file:
+
+        file_content = file.read()
+        storage.child('audio/' + 'test').put(file_content)
+
+    return '200'
 
 
 
