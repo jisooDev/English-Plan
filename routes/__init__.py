57f4e177c0d20d7e4f2fd04a68a0cb317678a9dc
jisooDev <phattarakorndev@gmail.com>
2023-04-06 13:31:48 +0700

    update listening admin 80%

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,6 +3,8 @@ import pyrebase
 import uuid
 import base64
 import query_helper as query
+import json
+btoa = lambda x:base64.b64decode(x)
 
 config = {
     "apiKey": "AIzaSyDExn5Hbtc_w4IT2-hUe-VT7XE7OkIT5b8",
--- routes/listening.py
+++ routes/listening.py
@@ -1,4 +1,3 @@
-from re import L
 from . import *
 
 blueprintListening = Blueprint('listening', __name__)
@@ -83,12 +82,6 @@ def create_writedown():
 
 
 
-@blueprintListening.route('/listening/interactive')
-def render_matching():
-    return render_template('admin/pages/listening/interactive.html')
-
-
-
 
 @blueprintListening.route('/listening/selectword')
 def render_selectword():
@@ -168,3 +161,81 @@ def del_selectword(_id):
         print(e)
 
     return redirect('/admin/listening/selectword')
+
+
+@blueprintListening.route('/listening/interactive')
+def render_matching():
+    connection = query.get_connection()
+    cursor = connection.cursor()
+
+    try :
+        sql_str = '''
+            SELECT id,difficulty,json_data,audio FROM listening_interactive_conversation
+        '''
+        cursor.execute(sql_str)
+        response = cursor.fetchall()
+        
+    except Exception as e :
+        print(e)
+
+    return render_template('admin/pages/listening/interactive.html' , response=response , json=json , atob=atob , btoa=btoa)
+
+@blueprintListening.route('/listening/interactive/create' , methods=[ 'GET' , 'POST'])
+def render_matching_create():
+
+    if request.method == 'POST':
+        try :
+            file = request.files['audio']
+            json_data = request.form.get('data')
+            difficulty = request.form.get('difficulty')
+
+            connection = query.get_connection()
+            cursor = connection.cursor()
+            _uuid = uuid.uuid4().hex
+
+
+            reformat_json = json.loads(json_data)
+
+            for item in reformat_json :
+                item['answer'] = atob(item['answer'])
+
+            file_data = file.read()
+            storage.child('lintening/interactive/%s'%_uuid).put(file_data)
+            _url = storage.child('lintening/interactive/%s'%_uuid).get_url(None)
+
+            sql_data = (_uuid , difficulty, _url , json.dumps(reformat_json))
+            sql_str = '''
+                INSERT INTO listening_interactive_conversation
+                (id , difficulty, audio, json_data)
+                VALUES
+                (%s , %s, %s ,%s)
+            '''
+            cursor.execute(sql_str, sql_data)
+            connection.commit()
+
+        except Exception as e :
+            print(e)
+
+        return redirect('/admin/listening/interactive')
+    else :
+        return render_template('admin/pages/listening/interactive_create.html')
+
+@blueprintListening.route('/listening/interactive/delete/<_id>')
+def del_matching(_id):    
+
+    connection = query.get_connection()
+    cursor = connection.cursor()
+
+    try :
+        sql_str = '''
+            DELETE FROM listening_interactive_conversation 
+            WHERE id = "%s";
+        '''%_id
+        cursor.execute(sql_str)
+        connection.commit()
+        storage_delete.delete("lintening/interactive/%s"%_id)
+
+    except Exception as e :
+        print(e)
+
+    return redirect('/admin/listening/interactive')
