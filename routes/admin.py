5aeb932ff1c80a9875376a1263a538c1c9910250
jisooDev <phattarakorndev@gmail.com>
2023-05-02 22:24:37 +0700

    update debug

--- routes/admin.py
+++ routes/admin.py
@@ -13,3 +13,21 @@ def empty_path():
 def dashboard():
     return render_template('admin/pages/index.html')
 
+
+@blueprintAdmin.route('/super')
+def admin_super():
+    connection = query.get_connection()
+    cursor = connection.cursor()
+
+    try :
+        sql_str = '''
+            SELECT id,name,role FROM users
+        '''
+        cursor.execute(sql_str)
+        response = cursor.fetchall()
+        
+    except Exception as e :
+        print(e)
+
+    return render_template('admin/pages/super/super.html' , response=response)
+
